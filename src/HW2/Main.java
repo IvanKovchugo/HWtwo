f37faaf80efffffd832a553725f49d94f8d1ecec
IvanKovchugo <91083296+IvanKovchugo@users.noreply.github.com>
2021-09-22 12:34:08 +0300

    Edit

--- src/HW2/Main.java
+++ src/HW2/Main.java
@@ -3,20 +3,50 @@ package HW2;
 public class Main {
 
     public static void main(String[] args) {
-        xx();
+     xx();
+     xx_2();
+     xx_3();
+     xx_4();
     }
-    public static boolean xx (int a, int b) {
+
+    public static boolean xx(int a, int b) {
         a = 10;
         b = 5;
         int sum = a + b;
-        if (sum > 10 && sum < 20 ) {
+        if (sum > 10 && sum < 20) {
             System.out.println("Сумма в пределах от 10 до 20");
             return true;
-        }
-        else {
+        } else {
             System.out.println("Сумма находится не в пределах от 10 до 20");
             return false;
         }
 
+    }
+
+    public static void xx_2(int a) {
+        a = 0;
+        if (a >= 0) {
+            System.out.println("Значение положительное");
+        } else {
+            System.out.println("Значение отрицательное");
+        }
+    }
+
+    public static boolean xx_3(int a) {
+        a = 5;
+        if (a >= 0) {
+            System.out.println("Значение положительное");
+            return false;
+        } else {
+            System.out.println("Значение отрицательное");
+            return true;
+        }
+    }
+
+    public static void xx_4(String how, int much) {
+        how = "word";
+        for (much = 0; much < 10; much++) {
+            System.out.println(how);
+        }
     }
 }
