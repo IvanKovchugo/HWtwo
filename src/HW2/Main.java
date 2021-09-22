3fd10985f80cff3680741ca755d12c12d98abeb0
IvanKovchugo <91083296+IvanKovchugo@users.noreply.github.com>
2021-09-22 12:11:51 +0300

    Initial commit

---
+++ src/HW2/Main.java
@@ -0,0 +1,16 @@
+package HW2;
+
+public class Main {
+
+    public static void main(String[] args) {
+    xx;
+    }
+    public static boolean xx (int a, int b) {
+        int sum = a + b;
+        if (sum > 10 && sum < 10 ) {
+
+        }
+        return true;
+
+    }
+}
